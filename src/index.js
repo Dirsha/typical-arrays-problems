b2e6a54a8f8940b19ebe508002d72b8c870dcd08
Igor Dirsha <idirsha@gmail.com>
2020-10-03 15:59:32 +0300

    feat: add slutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
+exports.min = function min(array) {
+    if (arguments.length && array.length) {
+        var minElement = array[0];
+        array.forEach((element) => {
+            if (element <= minElement) {
+                minElement = element;
+            }
+        });
+        return minElement;
+    } else return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (arguments.length && array.length) {
+        var maxElement = array[0];
+        array.forEach((element) => {
+            if (element >= maxElement) {
+                maxElement = element;
+            }
+        });
+        return maxElement;
+    } else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (arguments.length && array.length) {
+        var sum = 0;
+        var res;
+        for (var i = 0; i < array.length; i++) {
+            sum += array[i];
+            res = sum / array.length;
+        }
+        return res;
+    } else return 0;
+};
